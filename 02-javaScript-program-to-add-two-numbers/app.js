68ce7c934f18f23701e8f17582c3ce2fd1cf57cd
coderealistics <coderealistics@yandex.com>
2020-11-01 21:10:58 +0500

    JavaScript Program to Add Two Numbers!

--- 02-javaScript-program-to-add-two-numbers/app.js
+++ 02-javaScript-program-to-add-two-numbers/app.js
@@ -1,4 +1,9 @@
-let num1 = 5;
-let num2 = 3;
+// let num1 = 5;
+// let num2 = 3;
+// let sum = num1 + num2;
+// console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+let num1 = prompt("Enter the first number ");
+let num2 = prompt("Enter the second number ");
 let sum = num1 + num2;
-console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
